b97349cfa6c67298a458cafc436918453ec65974
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-10-19 12:17:25 +0200

    Add test

--- baseline.py
+++ baseline.py
@@ -19,13 +19,15 @@ torch.cuda.manual_seed_all(SEED)
 MODEL = "models_local/" + "Hate-speech-CNERG/dehatebert-mono-german" + "_model"
 TOKENIZER = "models_local/" + "Hate-speech-CNERG/dehatebert-mono-german" + "_tokenizer"
 
+
 def save_model_local():
     AutoModelForSequenceClassification.from_pretrained(
-            MODEL, num_labels=2, local_files_only=True
-        ).save_pretrained(f"models_local/{MODEL}_model")
+        MODEL, num_labels=2, local_files_only=True
+    ).save_pretrained(f"models_local/{MODEL}_model")
     AutoTokenizer.from_pretrained(
         TOKENIZER
     ).save_pretrained(f"models_local/{MODEL}_tokenizer")
+
 
 # %%
 class CommentDataset(torch.utils.data.Dataset):
@@ -37,15 +39,16 @@ class CommentDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, index):
         return (
-            self.data["kommentar"][index], 
+            self.data["kommentar"][index],
             torch.tensor(self.data["label"][index], dtype=torch.float32)
         )
-    
+
+
 def setup_data(debug_subset: Optional[int] = None):
     data_path = "./data/all_DEFR_comments_27062022.csv"
 
     df = pr.read_csv(data_path, dtypes={"ArticleID": pr.Utf8, "ID": pr.Utf8})
-    
+
     # TODO: Ask about the NULL values
     # TODO: Ask about the duplicates and remove them properly .unique(subset=["kommentar"])
     df = df.drop_nulls()
@@ -54,6 +57,7 @@ def setup_data(debug_subset: Optional[int] = None):
 
     data = df.select(["kommentar", "label"])
     return data
+
 
 def setup_datasets(data: pr.DataFrame, test_split: float = 0.2, val_split: float = 0.2):
     full_size = data.shape[0]
@@ -64,6 +68,7 @@ def setup_datasets(data: pr.DataFrame, test_split: float = 0.2, val_split: float
         CommentDataset(data), [train_size, val_size, test_size]
     )
     return train_dataset, val_dataset, test_dataset
+
 
 def setup_dataloader(data: torch.utils.data.Dataset, shuffle: bool, batch_size: int = 16):
     return torch.utils.data.DataLoader(
@@ -101,7 +106,8 @@ class BERTModule(torch.nn.Module):
         return self.model(x)
 
 
-def train_loop(model: torch.nn.Module, epochs: int, train_loader: torch.utils.data.DataLoader, val_loader: torch.utils.data.DataLoader, verbose: bool = False):
+def train_loop(model: torch.nn.Module, epochs: int, train_loader: torch.utils.data.DataLoader,
+               val_loader: torch.utils.data.DataLoader, verbose: bool = False):
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9, weight_decay=1e-5)
 
@@ -124,16 +130,15 @@ def train_loop(model: torch.nn.Module, epochs: int, train_loader: torch.utils.da
             )["input_ids"].to(device)
 
             batch_X, batch_Y = batch_X.to(device), batch_Y.to(device)
-            
+
             optimizer.zero_grad()
             if torch.cuda.is_available():
                 batch_X = batch_X.cuda()
                 batch_Y = batch_Y.cuda()
 
-            batch_X.cuda() if torch.cuda.is_available() else batch_X
             outputs = model(batch_X)
 
-            logits = outputs.logits.squeeze()
+            logits = outputs.logits
             batch_Y = torch.squeeze(batch_Y).long()
             loss = loss_fn(logits, batch_Y)
 
@@ -141,17 +146,21 @@ def train_loop(model: torch.nn.Module, epochs: int, train_loader: torch.utils.da
             optimizer.step()
 
             losses += loss.item()
-        
+
         if verbose:
-            print(f"Train loss after {epochs: 04d} epochs: {losses/((i+1)*len(train_loader)): 0.4f}")
-        
+            print(f"Train loss after {epochs: 04d} epochs: {losses / ((i + 1) * len(train_loader)): 0.4f}")
+
         if i % 10 == 0:
             validation(model, tokenizer, loss_fn, val_loader, epochs, device)
 
-def validation(model: torch.nn.Module, tokenizer, loss_fn: Callable, val_dataloader: torch.utils.data.DataLoader, epochs: int, device: str = "cpu"):
-    with torch.inference_mode():   
+    return model, tokenizer
+
+
+def validation(model: torch.nn.Module, tokenizer, loss_fn: Callable, val_dataloader: torch.utils.data.DataLoader,
+               epochs: int, device: str = "cpu"):
+    with torch.inference_mode():
         model.eval()
-        
+
         val_loss = 0
         targets, outs = [], []
         print("Validation:")
@@ -163,12 +172,10 @@ def validation(model: torch.nn.Module, tokenizer, loss_fn: Callable, val_dataloa
 
             outputs = model(valid_X)
             logits = outputs.logits
-            logits = torch.squeeze(logits)
-            #logits = torch.argmax(logits, dim=1)
-            
-            #outs.extend(logits > 0)
-            outs.extend([logits > 0])
-            #logits = torch.squeeze(logits)
+            # logits = torch.argmax(logits, dim=1)
+
+            # outs.extend(logits > 0)
+            outs.extend(np.argmax(logits.cpu().numpy(), axis=1))
 
             targets.extend(valid_Y)
             valid_Y = valid_Y.unsqueeze(0)
@@ -179,3 +186,30 @@ def validation(model: torch.nn.Module, tokenizer, loss_fn: Callable, val_dataloa
         # scheduler.step(val_loss)
         print(f"  Val loss after {epochs: 04d} epochs: {val_loss: 0.4f}")
 
+
+def test(model: torch.nn.Module, tokenizer, test_dataloader: torch.utils.data.DataLoader):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    with torch.inference_mode():
+        model.eval()
+        pred_Y, test_Y_all = [], []
+
+        print("Test:")
+        for test_X, test_Y in tqdm.tqdm(test_dataloader):
+            test_X = tokenizer.batch_encode_plus(
+                list(test_X), padding=True, truncation=True, return_tensors="pt"
+            )["input_ids"]
+            test_X, test_Y = test_X.to(device), test_Y.to(device)
+
+            outputs = model(test_X)
+            logits = outputs.logits
+            pred_Y.extend(np.argmax(logits.cpu().numpy(), axis=1))
+            test_Y_all.extend(test_Y.cpu().numpy())
+
+        pred_Y = np.array(pred_Y)
+        test_Y_all = np.array(test_Y_all)
+
+        acc = sum(pred_Y == test_Y_all) / len(test_Y_all)
+        print(f"Accuracy is {acc}")
+
+        return pred_Y, test_Y_all
--- test_baseline.py
+++ test_baseline.py
@@ -1,13 +1,17 @@
 from baseline import *
+import torch
 
 data = setup_data()
 train_dataset, val_dataset, test_dataset = setup_datasets(data)
 
 train_dataloader = setup_dataloader(train_dataset, shuffle=True)
 val_dataloader = setup_dataloader(val_dataset, shuffle=False)
-test_dataloader = setup_dataloader(test_dataset, shuffle=False)
+test_dataloader = setup_dataloader(test_dataset, shuffle=False, batch_size=1)
 
 model = BERTModule()
 
-train_loop(model=model, epochs=10, train_loader=train_dataloader, val_loader=val_dataloader, verbose = True)
-torch.save(model, "models_saved/run1.pt")
+epochs = 10
+model, tokenizer = train_loop(model=model, epochs=epochs, train_loader=train_dataloader, val_loader=val_dataloader, verbose=True)
+torch.save(model, f"models_saved/run_1_{epochs}.pt")
+
+test(model, tokenizer, test_dataloader)
