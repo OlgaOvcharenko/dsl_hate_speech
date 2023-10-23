c3f2d12909f00b36fda7e156bf944390241ddca9
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-10-23 10:25:42 +0200

    Fix langs

--- utils/create_lang_labels.py
+++ utils/create_lang_labels.py
@@ -4,8 +4,20 @@ from langdetect import detect
 def read_data_to_df(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
 
-def detect_language(df: pd.DataFrame, comments_col: str = "kommentar", lang_col: str = "lang") -> pd.DataFrame:
-    return pd.concat([df, pd.get_dummies([detect(str(val)) if isinstance(val, str)  and val.isalpha() else str(val) for val in  df[comments_col]])], axis=1)
+def detect_language(df: pd.DataFrame, comments_col: str = "kommentar_original", lang_col: str = "lang") -> pd.DataFrame:
+    # return pd.concat([df, pd.get_dummies([detect(str(val)) if isinstance(val, str)  and val.isalpha() else str(val) for val in  df[comments_col]])], axis=1)
+    # return [detect(str(val)) if isinstance(val, str) and val.isalpha() else str('de') for val in  df[comments_col]]
+    # return [detect(str(val)) if str(val) else 't' for val in  df[comments_col]]
+    langs = []
+    for val in  df[comments_col]:
+        try:
+            language = detect(val)
+        except:
+            language = "err"
+        langs.append(language)
+    df[lang_col] = langs
+    print(set(langs))
+    return df
 
 
 df = read_data_to_df("data/all_DEFR_comments_27062022.csv")
