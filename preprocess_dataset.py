cc5107b6e83e4127aa5d77f7289f5e7c757df635
eugleo <wybitul.evzen@gmail.com>
2023-10-23 20:40:20 +0200

    Add dataset preprocessing pipeline

---
+++ preprocess_dataset.py
@@ -0,0 +1,46 @@
+# %%
+import polars as pl
+import polars.selectors as cs
+from polars import col as c
+from transformers import AutoTokenizer
+
+# %%
+df = pl.read_csv(
+    "data/all_comments_lang.csv", dtypes={"ArticleID": pl.Utf8, "ID": pl.Utf8}
+)
+
+# %%
+df = (
+    df.drop_nulls()  # TODO Ask about the NULL values
+    .unique()  # TODO Ask about the duplicate rows
+    .drop("ArticleID", "ID", "kommentar_original", "toxische_sprache")
+    .filter(c("lang") != "fr")  # Remove the french comments
+    .rename(
+        {
+            "kommentar": "comment",
+            "geschlecht": "gender",
+            "alter": "age",
+            "sexualitaet": "sexuality",
+            "nationalitaet": "nationality",
+            "beeintraechtigung": "disability",
+            "sozialer_status": "social_status",
+            "politik": "political_views",
+            "aussehen": "appearance",
+            "andere": "other",
+        }
+    )  # Standardize the column names
+    .rename({"label": "toxic"})  # Fix the toxicity labels
+    .with_columns(
+        targeted=pl.sum_horizontal(cs.all().exclude(["toxic", "comment", "lang"]) > 0)
+    )  # Create a new column to indicate if the toxicity is targeted (# targets > 0)
+    .filter(
+        (c("targeted") == 0) | (c("toxic") == 1)
+    )  # TODO Ask about these illogical (targeted but non-toxic) rows
+    .cast(
+        {cs.numeric(): pl.Int64, cs.boolean(): pl.Int64}
+    )  # Cast the boolean variables to long
+    .with_row_count("id")  # Add a column with the row id
+)
+
+# %%
+df.write_csv("data/clean_comments_non-fr.csv")
