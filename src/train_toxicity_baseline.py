b17c5682b3431264c484fc27c2273e7e2473f5f1
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-11-01 00:58:21 +0100

    Fix model name

--- src/train_toxicity_baseline.py
+++ src/train_toxicity_baseline.py
@@ -26,7 +26,7 @@ training_config = {
 model_config = {
     "model_name": "toxicity-detection-baseline",
     "model_dir": f"/cluster/scratch/{user}/dsl_hate_speech/models",
-    "base_model_id": "Hate-speech-CNERG/dehatebert-mono-german_model",
+    "base_model_id": "Hate-speech-CNERG/dehatebert-mono-german",
     "layers_to_freeze": list(range(11)),
 }
 
