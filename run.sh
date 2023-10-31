60b468c2e7e3c760bcd24701bc9114e6f652880f
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-11-01 00:18:06 +0100

    Fix run

--- run.sh
+++ run.sh
@@ -8,6 +8,8 @@
 #SBATCH --mem-per-cpu=8G
 #SBATCH --time=4:00:00
 
+run_toxicity=true
+
 mkdir -p logs
 
 module load eth_proxy gcc/8.2.0 python_gpu/3.11.2
@@ -18,5 +20,10 @@ echo
 source ".venv/bin/activate"
 
 export WANDB__SERVICE_WAIT=300
-# python src/train_toxicity_baseline.py
-python src/train_target_group_baseline.py
+
+if [ "$run_toxicity" = true] ;
+then
+  python src/train_toxicity_baseline.py
+else
+  python src/train_target_group_baseline.py
+fi
