25f88252d81e7a3148e77294682b046bdb2985c8
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-11-21 12:35:32 +0100

    Fix dataset writing

--- src/dsl/llm.py
+++ src/dsl/llm.py
@@ -59,8 +59,8 @@ config = wandb.config
 
 # Step 2: Load the dataset
 df_train, df_eval = setup_datasets_2(config)
-for row in df_train.iter_rows(named=True):
-    with jsonlines.open("data/llm/train.jsonl", mode="w") as writer:
+with jsonlines.open("data/llm/train.jsonl", mode="w") as writer:
+    for row in df_train.iter_rows(named=True):
         text, label = row["comment_preprocessed_legacy"], row["toxic"]
         prompt = '''Toxic comment is any kind of offensive or denigrating speech against humans based on
         their identity (e.g., based on gender, age, nationality, political views, social views, sex, disability, appearance etc.).
@@ -71,8 +71,8 @@ for row in df_train.iter_rows(named=True):
         completion = " Yes" if label == 1 else " No"
         writer.write({"prompt": prompt, "completion": completion})
 
-for row in df_eval.iter_rows(named=True):
-    with jsonlines.open("data/llm/eval.jsonl", mode="w") as writer:
+with jsonlines.open("data/llm/eval.jsonl", mode="w") as writer:
+    for row in df_eval.iter_rows(named=True):
         text, label = row["comment_preprocessed_legacy"], row["toxic"]
         prompt = '''Toxic comment is any kind of offensive or denigrating speech against humans based on
         their identity (e.g., based on gender, age, nationality, political views, social views, sex, disability, appearance etc.).
