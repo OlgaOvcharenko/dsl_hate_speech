c3f9c3c9cc4f66eb75349959b549d960a53b80c4
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-12-03 20:58:11 +0100

    Add exho and change time

--- run.sh
+++ run.sh
@@ -6,11 +6,14 @@
 #SBATCH --gres=gpumem:32g
 #SBATCH --cpus-per-task=6
 #SBATCH --mem-per-cpu=12G
-#SBATCH --time=10:00:00
+#SBATCH --time=00:10:00
 
 mkdir -p logs
 # module load eth_proxy gcc/8.2.0 python_gpu/3.11.2
 source ".venv/bin/activate"
 
 export WANDB__SERVICE_WAIT=300
+
+echo "$1"
+
 python "$1"
