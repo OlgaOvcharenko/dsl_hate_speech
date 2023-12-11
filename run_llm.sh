fa69ce22c721266966d02eab22ba7baa30d125c4
Olga Ovcharenko <oovcharenko@eu-login-47.euler.ethz.ch>
2023-12-11 20:42:22 +0100

    Add all

--- run_llm.sh
+++ run_llm.sh
@@ -12,16 +12,16 @@ CONSUL_HTTP_ADDR=""
 
 mkdir -p logs
 module load eth_proxy gcc/11.4.0 python/3.11.6 cuda/12.1.1 
-source ".venv/bin/activate"
+source ".venv_llama/bin/activate"
 
 export WANDB__SERVICE_WAIT=300
-
+export TRANSFORMERS_CACHE=/cluster/scratch/oovcharenko/dsl_hate_speech/cache/
 echo "$(date)"
 echo "$1"
 
 nvidia-smi
 
-export CUDA_VISIBLE_DEVICES=0,1
+#export CUDA_VISIBLE_DEVICES=0,1
 
 # python -m torch.distributed.launch "$1"
 torchrun --standalone --nnodes=1 "$1"
--- setup_venv.sh
+++ setup_venv.sh
@@ -5,8 +5,8 @@ if [[ ! -d "python_env" ]]; then
 
   # HACK Maybe locally we don't want to use the system site packages
   module load gcc/11.4.0 python/3.11.6 cuda/12.1.1 
-  python -m venv .venv
-  source ".venv/bin/activate"
+  python -m venv .venv_llama
+  source ".venv_llama/bin/activate"
 
   pip install --upgrade pip
   pip install -r requirements.txt
