578cb625ba670039709f616d3a22eda8d06d8e30
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-12-12 19:43:15 +0100

    Add data

--- src/dsl/dataset.py
+++ src/dsl/dataset.py
@@ -145,9 +145,9 @@ def setup_datasets_2(config: wandb.Config):
 
 def setup_datasets_targets_only(config: wandb.Config, file):
     df, comments, labels = _load_data(file, config.class_names)
-    indices = df["targeted"] == 1
     df = df.to_pandas()
-    print(df[[indices]])
+    indices = df["targeted"] == 1
+    print(df[indices])
     # df = df[[indices]]
     df = df[indices]
     return df
