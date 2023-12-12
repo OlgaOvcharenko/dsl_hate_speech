44eda356afdd562d6b5683baa97238d8f56da041
Olga Ovcharenko <ovcharenko.folga@gmail.com>
2023-12-12 18:20:48 +0100

    Add changes

--- run_llm_2g.sh
+++ run_llm_2g.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --output=logs/log-%j.out
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=4
+#SBATCH --gpus-per-node=3
 #SBATCH --gres=gpumem:32G
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=10G
 #SBATCH --time=12:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
