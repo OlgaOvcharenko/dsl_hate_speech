e4cc28d6b4c01dd0e3696e76a49bcffe1b553a36
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2023-12-14 14:38:24 +0100

    Add files

--- src/dsl/llm_targets_inference.py
+++ src/dsl/llm_targets_inference.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 
 from dataset import setup_datasets_2, setup_datasets_targets_only
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+# os.environ["CUDA_VISIBLE_DEVICES"]="0"
 import torch
 import torch.nn as nn
 import bitsandbytes as bnb
@@ -27,15 +27,15 @@ config_local = {}
 with open("configs/defaults.yaml") as f:
     base_config = yaml.load(f, Loader=yaml.FullLoader)
     config_local.update(base_config)
-with open("configs/toxicity/defaults.yaml") as f:
+with open("configs/targets/defaults.yaml") as f:
     toxicity_config = yaml.load(f, Loader=yaml.FullLoader)
     config_local.update(toxicity_config)
 
 config_local.update(
     {
         "model_directory": f"/cluster/scratch/{user}/dsl_hate_speech/models",
-        "train_data": "data/processed_comments_train_v3.csv",
-        "evaluation_data": "data/processed_comments_evaluation_v3.csv",
+        "train_data": "data/processed_training_main_v4.csv",
+        "evaluation_data": "data/processed_evaluation_main_v4.csv",
         "model": "toxicity-detection-llm",
         "early_stopping_enabled": False,
         "early_stopping_epoch": 3,
@@ -44,7 +44,7 @@ config_local.update(
         "epochs": 5,
     }
 )
-wandb.init(project="toxicity-detection-llm", config=config_local)
+wandb.init(project="targets-detection-llm", config=config_local)
 
 match wandb.config["base_model"]:
     case "Hate-speech-CNERG/dehatebert-mono-german":
@@ -56,15 +56,17 @@ match wandb.config["base_model"]:
 config_local = wandb.config
 
 
-model_path = "meta-llama/Llama-2-7b-hf"
+model_path = "outputs_targets/"
 model = AutoModelForCausalLM.from_pretrained( 
     model_path,
-    # load_in_8bit=True, 
-    device_map='auto',
+    #device_map='auto',
     quantization_config = BitsAndBytesConfig(
-        load_in_8bit=False, 
-        load_in_4bit=True
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
     ),
+    device_map={'':torch.cuda.current_device()},
     torch_dtype = torch.bfloat16
 )
 
@@ -77,7 +79,7 @@ for param in model.parameters():
     # cast the small parameters (e.g. layernorm) to fp32 for stability
     param.data = param.data.to(torch.float32)
 
-model.gradient_checkpointing_enable()  # reduce number of stored activations
+model.gradient_checkpointing_enable(gradient_checkpointing_kwargs={"use_reentrant": False})  # reduce number of stored activations
 model.enable_input_require_grads()
 
 class CastOutputToFloat(nn.Sequential):
@@ -110,93 +112,38 @@ config = LoraConfig(
 model = get_peft_model(model, config)
 print_trainable_parameters(model)
 
-max_memory = get_balanced_memory(
-    model,
-    max_memory=None,
-    no_split_module_classes=["DecoderLayer", "Attention", "MLP", "LayerNorm", "Linear"],
-    dtype='float16',
-    low_zero=False,
-)
-
-device_map = infer_auto_device_map(
-    model,
-    max_memory=max_memory,
-    no_split_module_classes=["DecoderLayer", "Attention", "MLP", "LayerNorm", "Linear"],
-    dtype='float16'
-)
-
-model = dispatch_model(model, device_map=device_map)
-
-
-df_train, df_eval = setup_datasets_targets_only(config_local)
-with jsonlines.open("data/llm_target/train.jsonl", mode="w") as writer:
-    for row in df_train.iter_rows(named=True):
-        text = row["comment_preprocessed_legacy"]
-        target_categories = ["gender", "age", "sexuality", "religion", "nationality", "disability", "social_status", "political_views", "appearance", "other"]
-        
-        curr_targets = ""
-        for val in target_categories:
-           if row[val] == 1:
-              val_fix = val.replace("_", " ")
-              curr_targets = curr_targets + val_fix + ", "
-        
-        if len(curr_targets) > 2:
-           curr_targets = curr_targets[:-2]
-
-
-        prompt = '''INSTRUCTION: Hate speech is any kind of offensive or denigrating speech against humans based on their identity. 
-        Hate speech can be targeted towards gender, age, sexuality, religion, nationality, disability, social status, political views, appearance, or other characteristic.
-        \nINPUT: What is 1 or more targets of this comment "{}"? 
-        Use only the following targets: gender, age, sexuality, religion, nationality, disability, social status, political views, appearance, other. \nOUTPUT: "{}".'''.format(
-            text, curr_targets
-        )
-
-        writer.write({"text": prompt})
-
-with jsonlines.open("data/llm_target/validation.jsonl", mode="w") as writer:
-    for row in df_eval.iter_rows(named=True):
-        text = row["comment_preprocessed_legacy"]
-        target_categories = ["gender", "age", "sexuality", "religion", "nationality", "disability", "social_status", "political_views", "appearance", "other"]
-        
-        curr_targets = ""
-        for val in target_categories:
-           if row[val] == 1:
-              val_fix = val.replace("_", " ")
-              curr_targets = curr_targets + val_fix + ", "
-        
-        if len(curr_targets) > 2:
-           curr_targets = curr_targets[:-2]
-
-
-        prompt = '''INSTRUCTION: Hate speech is any kind of offensive or denigrating speech against humans based on their identity. 
-        Hate speech can be targeted towards gender, age, sexuality, religion, nationality, disability, social status, political views, appearance, or other characteristic.
-        \nINPUT: What is 1 or more targets of this comment "{}"? 
-        Use only the following targets: gender, age, sexuality, religion, nationality, disability, social status, political views, appearance, other. \nOUTPUT: "{}".'''.format(
-            text, curr_targets
-        )
-
-        writer.write({"text": prompt})
-
+df_eval = setup_datasets_targets_only(config_local, file=config_local.evaluation_data)
+targets_cat = []
 with jsonlines.open("data/llm_target/test.jsonl", mode="w") as writer:
     for row in df_eval.iter_rows(named=True):
         text = row["comment_preprocessed_legacy"]
+        target_categories = ["gender", "age", "sexuality", "religion", "nationality", "disability", "social_status", "political_views", "appearance", "other"]
+        if len(text) < 500:
+            curr_targets = ""
+            for val in target_categories:
+                if row[val] == 1:
+                    targets_cat.append(val)
+                    val_fix = val.replace("_", " ")
+                    curr_targets = curr_targets + val_fix + ", "
+            
+            if len(curr_targets) > 2:
+                curr_targets = curr_targets[:-2]
 
-        prompt = '''INSTRUCTION: Hate speech is any kind of offensive or denigrating speech against humans based on their identity. 
-        Hate speech can be targeted towards gender, age, sexuality, religion, nationality, disability, social status, political views, appearance, or other characteristic.
-        \nINPUT: What is 1 or more targets of this comment "{}"? 
-        Use only the following targets: gender, age, sexuality, religion, nationality, disability, social status, political views, appearance, other.'''.format(
-            text
-        )
 
-        writer.write({"text": prompt})
+            prompt = '''INSTRUCTION: Hate speech is any kind of offensive or denigrating speech against humans based on their identity. 
+            Hate speech can be targeted towards gender, age, sexuality, religion, nationality, disability, social status, political views, appearance, or other characteristic.
+            \nINPUT: What is 1 or more targets of this comment "{}"? 
+            Use only the following targets: gender, age, sexuality, religion, nationality, disability, social status, political views, appearance, other.'''.format(
+                text
+            )
+
+            writer.write({"text": prompt})
 
 data = load_dataset("data/llm_target/")
-print(data)
 data = data.map(lambda samples: tokenizer(samples['text']), batched=True)
-print(data)
 
 training_args = transformers.TrainingArguments(
-        num_train_epochs=3,
+        num_train_epochs=5,
         per_device_train_batch_size=4, 
         gradient_accumulation_steps=4,
         warmup_steps=100, 
@@ -204,9 +151,8 @@ training_args = transformers.TrainingArguments(
         learning_rate=2e-4, 
         fp16=True,
         logging_steps=1, 
-        output_dir='outputs'
+        output_dir='outputs_targets'
     )
-
 
 trainer = transformers.Trainer(
     model=model, 
@@ -219,15 +165,18 @@ trainer = transformers.Trainer(
 print("parallel_mode: ", training_args.parallel_mode)
 print("n_gpus: ", training_args.n_gpu)
 
-model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+model.config.use_cache = True  # silence the warnings. Please re-enable for inference!
 with torch.autocast("cuda"):
-    trainer.train()
-    res = trainer.evaluate()
-    print(res)
+    print(data["test"])
 
-p, l, m = trainer.predict(data["test"])
-np.savetxt("data/predict_targets.csv", p, delimiter = ",")
+    p, l, m = trainer.predict(data["test"])
+    np.savetxt("data/predict_binary.csv", p, delimiter = ",")
 
+# # Inference
+# data = load_dataset("data/llm/eval")
+# print(data)
+# data = data.map(lambda samples: tokenizer(samples['quote']), batched=True)
+# print(data)
 # text = "Gute Idee und die Hassprediger auch gleich ausweisen und keine dieser Sorte mehr ins Land lassen. Gleichzeitig aber auch ein europaweites Verzeichnis pädophiler Pfaffen und diesen ein Berufsverbot auferlegen. Wird endlich Zeit dass in diesen Religionen aufgeräumt wird!"
 # prompt = '''INSTRUCTION: Toxic comment is any kind of offensive or denigrating speech against humans based on
 #         their identity (e.g., based on gender, age, nationality, political views, social views, sex, disability, appearance etc.).
